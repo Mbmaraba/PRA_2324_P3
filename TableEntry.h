fdaa185b50e25d8e12da040a164a81386e315fe6
MBMARABA <mbmaraba@alumno.upv.es@ldsic-vdi08.upvnet.upv.es>
2025-11-26 01:24:45 +0100

    Añadido Makefile y código de test de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -25,7 +25,7 @@ public:
     }
 
     friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
-        out << "[" << te.key << " -> " << te.value << "]";
+        out << "('" << te.key << "' => " << te.value << ")";
         return out;
     }
 };
