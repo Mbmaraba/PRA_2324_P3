8a53c2ef5a611ec5d04ca15bf0ad70aaf042848c
MBMARABA <mbmaraba@alumno.upv.es@ldsic-vdi17.upvnet.upv.es>
2025-12-11 22:56:10 +0100

    Actualizado Makefile y añadido código de test de la clase HashTable

--- HashTable.h
+++ HashTable.h
@@ -11,69 +11,69 @@ template <typename V>
 class HashTable : public Dict<V> {
 
 private:
-    int _capacity;
-    int _entries;
-    ListLinked<TableEntry<V>>* buckets;
+    int n; // número de elementos
+    int max; // tamaño de la tabla
+    ListLinked<TableEntry<V>>* table; // array de listas
 
-    int hash(std::string key) const {
-        unsigned int h = 0;
-        for (char c : key)
-            h = (h * 31 + c);
-        return h % _capacity;
+    int h(std::string key) const {
+        int sum = 0;
+        for (int i = 0; i < key.size(); i++)
+            sum += int(key.at(i));
+        return sum % max;
     }
 
 public:
 
-    HashTable(int capacity = 3) {
-        _capacity = capacity;
-        _entries = 0;
-        buckets = new ListLinked<TableEntry<V>>[_capacity];
+    HashTable(int size) {
+        max = size;
+        n = 0;
+        table = new ListLinked<TableEntry<V>>[max];
     }
 
     ~HashTable() {
-        delete[] buckets;
+        delete[] table;
     }
-
+    //por el Dict.h
     int entries() override {
-        return _entries;
+        return n;
     }
 
     int capacity() const {
-        return _capacity;
+        return max;
     }
 
     // ----------------------
-    // INSERT
+    // INSERT   del Dict
     // ----------------------
     void insert(std::string key, V value) override {
-        int index = hash(key);
+        int index = h(key);//calculamos indice
+        TableEntry<V> te(key, value);//se crea objeto
 
-        TableEntry<V> te(key, value);
-
-        int pos = buckets[index].search(te);
+        int pos = table[index].search(te);//Se busca en la lista enlazada correspondiente a table[index]
         if (pos != -1)
             throw std::runtime_error("Key '" + key + "' already exists!");
-
-        buckets[index].prepend(te);
-        _entries++;
+        //se inserta el nuevo TableEntry al principio de la lista enlazada (prepend)
+        table[index].prepend(te);
+        n++;
     }
 
     // ----------------------
-    // SEARCH
+    // SEARCH: Busca el valor correspondiente a key.
+    //Devuelve el valor value asociado si key está en el diccionario.
     // ----------------------
     V search(std::string key) override {
-        int index = hash(key);
-
-        TableEntry<V> te(key);
-        int pos = buckets[index].search(te);
-
-        if (pos == -1)
+        int index = h(key);
+        TableEntry<V> te(key);//crea objeto temporal con clave key
+        //table == cubeta
+        //Busca en la lista enlazada de la cubeta index un TableEntry con la misma clave
+        int pos = table[index].search(te);
+        if (pos == -1)//busqueda menos 1
             throw std::runtime_error("Key '" + key + "' not found!");
 
-        return buckets[index].get(pos).value;
+        return table[index].get(pos).value;//se obtiene el elemento en la posición pos dentro de la lista enlazada.
     }
 
-    // Para test: dict["Four"]
+    // operator[] para tests
     V operator[](std::string key) {
         return search(key);
     }
@@ -82,35 +82,34 @@ public:
     // REMOVE
     // ----------------------
     V remove(std::string key) override {
-        int index = hash(key);
+        int index = h(key);//calcula en indice de la cubeta
+        TableEntry<V> te(key);//crea objeto temporal
 
-        TableEntry<V> te(key);
-        int pos = buckets[index].search(te);
-
+        int pos = table[index].search(te);
         if (pos == -1)
             throw std::runtime_error("Key '" + key + "' not found!");
 
-        TableEntry<V> removed = buckets[index].remove(pos);
-        _entries--;
+        TableEntry<V> removed = table[index].remove(pos);
+        n--;
         return removed.value;
     }
 
     // ----------------------
-    // PRINT (exacto al test)
+    // PRINT
     // ----------------------
     void print(std::ostream& out) const {
-        out << "HashTable [entries: " << _entries
-            << ", capacity: " << _capacity << "]\n";
+        out << "HashTable [entries: " << n
+            << ", capacity: " << max << "]\n";
         out << "==============\n\n";
 
-        for (int i = 0; i < _capacity; i++) {
+        for (int i = 0; i < max; i++) {
             out << "== Cubeta " << i << " ==\n\n";
             out << "List => [\n";
 
-            for (int j = 0; j < buckets[i].size(); j++) {
-                const TableEntry<V>& e = buckets[i].get(j);
+            for (int j = 0; j < table[i].size(); j++) {
+                const TableEntry<V>& e = table[i].get(j);
                 out << "('" << e.key << "' => " << e.value << ")";
-                if (j < buckets[i].size() - 1)
+                if (j < table[i].size() - 1)
                     out << "\n  ";
             }
 
@@ -119,7 +118,7 @@ public:
 
         out << "==============\n\n";
     }
-
+    //para imprimir el contenido
     friend std::ostream& operator<<(std::ostream& out, const HashTable<V>& ht) {
         ht.print(out);
         return out;
