5eae9b506ee9f5c2baf20b8e703d33ef2a43e224
MBMARABA <mbmaraba@alumno.upv.es@ldsic-vdi17.upvnet.upv.es>
2025-12-11 22:53:59 +0100

    Añadido Makefile y código de test de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -7,28 +7,40 @@
 template <typename V>
 class TableEntry {
 public:
-    std::string key;
-    V value;
+    std::string key;//El elemento clave del par
+    V value;//El elemento valor del par
 
     // Constructores
-    TableEntry(std::string key, V value) : key(key), value(value) {}
-    TableEntry(std::string key) : key(key) {}
-    TableEntry() : key("") {}
+    TableEntry(std::string key, V value) : key(key), value(value) {}//connclave y valor
+    TableEntry(std::string key) : key(key) {}//solo con clave (sin valor)
+    TableEntry() : key("") {}//cadena vacía) y sin valor.
 
     // Sobrecarga de operadores
+    //devuelve true si las claves son iguales
     friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-        return te1.key == te2.key;
+        return te1.key == te2.key;//SOLO se compara la clave
     }
 
+    //Devuelve true si las claves son distintas
     friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
         return te1.key != te2.key;
     }
 
+    //NUEVO ->requerido para BSTree
+    friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key < te2.key;   // orden lexicográfico
+    }
+    //compara clave(key)
+    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key > te2.key;   // orden lexicográfico
+    }
+
+    // Print se imprime con "std::cout << t;"
     friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
         out << "('" << te.key << "' => " << te.value << ")";
         return out;
     }
 };
 
-#endif // TABLEENTRY_H
+#endif //
 
